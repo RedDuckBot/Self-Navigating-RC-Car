929728f1bc1222405fa548b296566171c8a624bc
Dante Guy <dshillin11799@gmail.com>
2023-11-13 02:19:36 -0700

    config file changes

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,26 +1,30 @@
 
 mode=$1 
 
-if [ "$mode" = "manual" ]
+if [ "$mode" = "regular" ]
 then
+
+    echo "Regular setup."
+    docker run --runtime nvidia -it --rm \
+        --network host \
+        --volume /home/macnav/CMPT496/MacEwan-Navigation-Bot/ws:/mac_everything/ws \
+        --name host_mac_bot \
+        macnav
+
+else
     #Manual mode requires device mounting for controller (js*)
     #Note: if wanting to do stuff with turtlesim then device event* for 
     #controller must be mounted instead of js*
     echo "Setup for Manual mode."
         #--device=/dev/input/event2 \
         #--device=/dev/input/js0 \
-        #--volume /home/macnav/CMPT496/MacEwan-Navigation-Bot/Config_Files/cyclonedds.xml:/tmp/cyclonedds.xml \
+        #--device=/dev/ttyUSB1 \
     docker run --runtime nvidia -it --rm \
         --network host \
         --volume /home/macnav/CMPT496/MacEwan-Navigation-Bot/ws:/mac_everything/ws \
         --device=/dev/ttyACM0 \
-        --name host_mac_bot \
-        macnav
-else
-    echo "Regular setup."
-    docker run --runtime nvidia -it --rm \
-        --network host \
-        --volume /home/macnav/CMPT496/MacEwan-Navigation-Bot/ws:/mac_everything/ws \
+        --device=/dev/ttyUSB0 \
+        --device=/dev/video0 \
         --name host_mac_bot \
         macnav
 fi
