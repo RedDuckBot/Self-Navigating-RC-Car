3bdf8d240b37992d8c4e10d928e55638d5c77be7
DanSimons <94565652+DanSimons@users.noreply.github.com>
2023-11-15 14:46:21 -0700

    Delete ws/build directory

--- ws/build/macnav_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dan/MacEwan-Navigation-Bot/ws/src/macnav_description
-# Build directory: /home/dan/MacEwan-Navigation-Bot/ws/build/macnav_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ws/build/manual_control/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/mac_everything/ws/install/mac_messages/share/mac_messages/package.sh"
